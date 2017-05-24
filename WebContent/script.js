3a9b6eedf8df855c55c0b8ff75f25dc89874693a
Anuja M <anujamanoharan@gmail.com>
2017-05-24 22:26:36 +0530

    login button click handled

--- WebContent/script.js
+++ WebContent/script.js
@@ -4,3 +4,32 @@ function login(ele){
 function signup(ele){
 	$("#signup").fadeToggle("slow");
 }
+function showMore(ele){
+	$("#more").fadeToggle("slow");
+}
+function confirmLogin(){
+	var email = $("#loginEmail").val();
+	var password = $("#loginPassword").val();
+	if(password == ""){
+		alert("password cannot be empty");
+		return false;
+	}
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if(!re.test(email)){
+		alert("Email is incorrect");
+		return false;
+	}
+	if(password != "anuja"){
+		alert("Password is incorrect");
+		return false;
+	}
+	loginUser(email, password);
+}
+function clearEmailField(){
+	if($("#loginEmail").val() == "example@example.com"){
+		$("#loginEmail").val("");
+	}
+}
+function loginUser(email, password){
+	
+}
