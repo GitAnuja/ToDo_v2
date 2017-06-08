502da597b11dae96417c6dff0dfe69c296bde910
Anuja M <anujamanoharan@gmail.com>
2017-06-08 09:51:02 +0530

    Signup button

--- WebContent/script.js
+++ WebContent/script.js
@@ -40,11 +40,40 @@ function loginUser(email, password){
 	var div = "<div class='link'>Hi "+username+"!</div>";
 	$(".row").children().last().html(div);
 	$("#login").hide();
+	$("#signup").hide();
 }
 function onloadFunc(){
-	$("#login input").on("keyup", function(e){
+	$("#login,#signup input").on("keyup", function(e){
 		if(e.keyCode == 13){
 			confirmLogin();			
 		}
 	});
+}
+function confirmSubmit(){
+	var name = $("#signupName").val();
+	var email = $("#signupEmail").val();
+	var password = $("#signupPassword").val();
+	var confirm = $("#confirmPassword").val();
+	if(name == ""){
+		alert("Name cannot be empty");
+		return false;
+	}
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if(!re.test(email)){
+		alert("Email is incorrect");
+		return false;
+	}
+	if(password == "" || confirm == ""){
+		alert("Enter password");
+		return false;
+	}
+	if(password.length < 8){
+		alert("Password should be minimum 8 characters");
+		return false;
+	}
+	if(password != confirm){
+		alert("Passwords do not match");
+		return false;
+	}
+	loginUser(email, password);
 }
