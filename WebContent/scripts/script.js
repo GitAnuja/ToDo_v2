f4f5f21a5394f41d5db42244a26f60bed0ced7a2
Anuja M <anujamanoharan@gmail.com>
2017-06-18 09:50:35 +0530

    handlebars

---
+++ WebContent/scripts/script.js
@@ -0,0 +1,121 @@
+function login(ele){
+	hideMenu();
+	$("#login .inputBox2").offset({top : $(".row").outerHeight(), left : $(window).width() - $("#login").outerWidth()});
+	$("#login").fadeToggle("slow");
+	event.stopPropagation();
+}
+function signup(ele){
+	hideMenu();
+	$("#signup .inputBox2").offset({top : $(".row").outerHeight(), left : $(window).width() - $("#signup").outerWidth()});
+	$("#signup").fadeToggle("slow");
+	event.stopPropagation();
+}
+function showMore(ele){
+	hideMenu();
+	$("#more .inputBox2").offset({top : $(".row").outerHeight(), left : $(ele).offset().left});
+	$("#more").fadeToggle("slow");
+	event.stopPropagation();
+}
+function confirmLogin(){
+	var email = $("#loginEmail").val();
+	var password = $("#loginPassword").val();
+	if(password == ""){
+		alert("password cannot be empty");
+		return false;
+	}
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if(!re.test(email)){
+		alert("Email is incorrect");
+		return false;
+	}
+	if(password != "anuja"){
+		alert("Password is incorrect");
+		return false;
+	}
+	loginUser(email, password);
+}
+function clearEmailField(){
+	if($("#loginEmail").val() == "example@example.com"){
+		$("#loginEmail").val("");
+	}
+}
+function loginUser(email, password){
+	var username = "Anuja";
+	$(".row").children().last().prev().css("display", "none");
+	var div = "<div class='link'>Hi "+username+"!</div>";
+	$(".row").children().last().html(div);
+	$("#login").hide();
+	$("#signup").hide();
+}
+function onloadFunc(){
+	$("#login,#signup input").on("keyup", function(e){
+		if(e.keyCode == 13){
+			confirmLogin();			
+		}
+	});
+//	var routes = {
+//	        '/todo' : function(){
+//	        	todo();
+//	        }
+//	      };
+//	var router = Router(routes);
+//    router.init();
+	$(document).on("click", hideMenu);
+	var se = document.createElement("script");
+	se.src = "scripts/todo.js";
+	var s1 = document.getElementsByTagName("script")[0];
+	s1.parentNode.insertBefore(se, s1);	
+}
+function confirmSubmit(){
+	var name = $("#signupName").val();
+	var email = $("#signupEmail").val();
+	var password = $("#signupPassword").val();
+	var confirm = $("#confirmPassword").val();
+	if(name == ""){
+		alert("Name cannot be empty");
+		return false;
+	}
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if(!re.test(email)){
+		alert("Email is incorrect");
+		return false;
+	}
+	if(password == "" || confirm == ""){
+		alert("Enter password");
+		return false;
+	}
+	if(password.length < 8){
+		alert("Password should be minimum 8 characters");
+		return false;
+	}
+	if(password != confirm){
+		alert("Passwords do not match");
+		return false;
+	}
+	loginUser(email, password);
+}
+function more(ele){
+	if(ele == "home"){
+		window.history.pushState({}, '', "#/");
+	}
+	else{
+		window.history.pushState({}, '', "#/"+ele);		
+	}
+//	$(window).trigger("hashchange",)
+	$("#more").hide();
+	if(ele == "todo"){
+		todo();		
+	}
+	else if(ele == "home"){
+		home();
+	}
+}
+function todo(){
+	$("#show").html(Handlebars.templates.todo);
+}
+function home(){
+	$("#show").html("");
+}
+function hideMenu(){
+	$(".inputBox").hide();
+}
---
+++ WebContent/scripts/todo.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['todo'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "ANUJA";
+},"useData":true});
+})();
